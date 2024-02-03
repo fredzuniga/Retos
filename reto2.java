b03e450397fa013a6d5a1ae0c56ac685cdd89303
alanbalfonso <a.bauzaa27@gmail.com>
2024-02-02 18:23:01 -0600

    correccion de error de variables globales y locales

--- reto2.java
+++ reto2.java
@@ -45,10 +45,13 @@ public class reto2 {
         //variables de registro de administrador y operaciones totales
         private String admin_usuario, admin_password;
         private int num_compras = 0;
+        private int num_ventas = 0;
         private int num_operaciones = 0;
         //egresos e ingresos por compras y ventas
         private double egresos_compras = 0;
         private double ingresos_ventas = 0;
+        //monto total
+        private double monto_total = 0;
         
         //constructor donde se declaran variables de usuario y contrasenia
         public ventaCajas(String user, String pass){
@@ -67,7 +70,7 @@ public class reto2 {
                 pass = sc.next();
                 //cuando el usuario y contraseña son correctos correr programa
                 if(admin_usuario.equals(usuario) == true && (admin_password.equals(pass) == true)){
-                    System.out.println("Inicio de sesion");
+                    System.out.println("------GRACIAS POR INICIAR SESION " + admin_usuario + "------");
                     break;
                 }
                 else{
@@ -87,37 +90,48 @@ public class reto2 {
                 switch(opcion){
                     case 'A' ->{
                         //variables exclusivas de esta opcion
-                        int num_compras;
+                        int num_cajas;
                         double precio;
                         double monto;
-                        System.out.println("Bienvenido a la seccion de compra de cajas."
+                        System.out.println("------------------------------\nBienvenido a la seccion de compra de cajas."
                                 + "\nEscribe las cajas que vas a comprar: ");
-                        num_compras = sc.nextInt();
+                        num_cajas = sc.nextInt();
                         System.out.println("Escribe el precio de cada caja: ");
                         precio = sc.nextFloat();
-                        monto = precio * num_compras;
-                        System.out.println("La compra realizada por " + num_compras + " cajas al costo: $" + precio + " es igual a: $" + monto);
+                        monto = precio * num_cajas;
+                        System.out.println("La compra realizada por " + num_cajas + " cajas al costo: $" + precio + " es igual a: $" + monto
+                        + "\n-----------------------------");
                         //la variable de compras totales aumenta cada vez que esta operacion se realice
-                        num_compras ++;
+                        num_compras++;
                         //la variable num_operaciones funciona para guardar todas las operaciones realizadas durante el programa
-                        num_operaciones ++;
+                        num_operaciones++;
                         //los egresos se guardan y se restan por cada compra realizada
-                        egresos_compras = egresos_compras - monto;
+                        egresos_compras = egresos_compras + monto;
+                        monto_total = monto_total - monto;
                     }
                     case 'B' ->{
-                        int num_ventas;
+                        int num_cajas;
                         double precio_ventas;
                         double monto_ventas;
-                        System.out.println("Bienvenido a la seccion de venta de cajas \nEscribe las cajas que vas a vender: ");
-                        num_ventas = sc.nextInt();
+                        System.out.println("------------------------------\nBienvenido a la seccion de venta de cajas \nEscribe las cajas que vas a vender: ");
+                        num_cajas = sc.nextInt();
                         System.out.println("Escribe el precio de cada caja: ");
                         precio_ventas = sc.nextFloat();
-                        monto_ventas = precio_ventas * num_ventas;
-                        System.out.println("La venta realizada por " + num_ventas + " cajas al costo: $" + precio_ventas + " es igual a: $" + monto_ventas);
-                        num_ventas ++;
-                        num_operaciones ++;
+                        monto_ventas = precio_ventas * num_cajas;
+                        System.out.println("La venta realizada por " + num_cajas + " cajas al costo: $" + precio_ventas + " es igual a: $" + monto_ventas
+                        + "\n-----------------------------");
+                        num_ventas++;
+                        num_operaciones++;
                         ingresos_ventas = ingresos_ventas + monto_ventas;
-                        
+                        monto_total = monto_total + monto_ventas;
+                    }
+                    case 'C' ->{
+                        System.out.println("------------------------------\nReporte de operaciones: \n1.- Cantidad total de ventas: " 
+                        + num_ventas + "\n2.- Cantidad total de compras: " + num_compras + 
+                        "\n3.- Ingresos generados por ventas: " + ingresos_ventas + 
+                        "\n4.- Egresos generados por compras: " + egresos_compras + 
+                        "\n5.- Monto en la caja: " + monto_total
+                        + "\n------------------------------");
                     }
                     case 'S' ->{
                         break;
@@ -126,8 +140,12 @@ public class reto2 {
                 }
             }
                 //codigo de salida
-                System.out.println("Reporte de control:");
-
+                System.out.println("Reporte de operaciones final: \n1.- Cantidad total de ventas: " 
+                + num_ventas + "\n2.- Cantidad total de compras: " + num_compras + 
+                "\n3.- Ingresos generados por ventas: " + ingresos_ventas + 
+                "\n4.- Egresos generados por compras: " + egresos_compras + 
+                "\n5.- Monto en la caja: " + monto_total + "\n6.- Cantidad total de operaciones realizadas: " + num_operaciones
+                + "\n------MUCHAS GRACIAS POR SU PREFERENCIA VUELVA PRONTO------");
                 }
     }
 }
