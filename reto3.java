e645a9b16331b3b7c713d477cc9b532cc63f952b
alanbalfonso <a.bauzaa27@gmail.com>
2024-02-03 14:43:08 -0600

    Creacion de boceto para empezar en el reto 3

--- reto3.java
+++ reto3.java
@@ -1,3 +1,26 @@
+//Reto 3    **Diccionario y matriz**    Alan Bauza
+import java.io.*;
+import java.util.Scanner;
 public class reto3 {
-    
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        System.out.println("Escoge el programa que quieres ejecutar: \n1.- Simulador de diccionario."
+        + "\n2.- Contador de numeros pares e impares.");
+        opcion = sc.nextInt();
+        switch (opcion) {
+            case 1->{
+                System.out.println("--------DICCIONARIO--------"
+                +"\nBienvenido al diccionario, escoge una opcion: "
+                +"\nA. Listar palabras del diccionario."
+                +"\nB. Buscar palabra en el diccionario."
+                +"\nC. Detalle del diccionario."
+                +"\nS. Finalizar programa.");
+            }
+            case 2->{
+
+            }
+            default -> System.out.println("Por favor elija una opcion disponible");
+        }
+    }
 }
