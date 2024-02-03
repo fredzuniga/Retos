39f1230a279a287bc04e65cc6272a8aba7f86104
alanbalfonso <a.bauzaa27@gmail.com>
2024-02-03 15:42:45 -0600

    agregado caso de salida, mejorada la logica

--- reto3.java
+++ reto3.java
@@ -5,20 +5,57 @@ public class reto3 {
     public static void main(String[] args) throws IOException{
         Scanner sc = new Scanner(System.in);
         int opcion;
+        //mensaje de inicio para escoger la opcion
         System.out.println("Escoge el programa que quieres ejecutar: \n1.- Simulador de diccionario."
         + "\n2.- Contador de numeros pares e impares.");
         opcion = sc.nextInt();
         switch (opcion) {
             case 1->{
+                //programa de diccionario
+                char opcion_diccionario = 'x';
+
                 System.out.println("--------DICCIONARIO--------"
                 +"\nBienvenido al diccionario, escoge una opcion: "
                 +"\nA. Listar palabras del diccionario."
                 +"\nB. Buscar palabra en el diccionario."
                 +"\nC. Detalle del diccionario."
                 +"\nS. Finalizar programa.");
+                opcion_diccionario = sc.next().charAt(0);
+                while(opcion_diccionario != 'S'){
+                    switch (opcion_diccionario) {
+                        case 'A' ->{
+    
+                        }
+                        case 'B' ->{
+    
+                        }
+                        case 'C' ->{
+    
+                        }
+                        case 'S' ->{
+                            break;  //rompe el while
+                        }
+                        default -> System.out.println("Elije una opcion disponible");
+                    }
+                }
+                //codigo de salida del while
+                System.out.println("-----FIN DEL PROGRAMA-----");
+
             }
             case 2->{
+                //programa de matriz
+                int opcion_filas;
+                int opcion_columnas;
 
+                System.out.println("-----CONTADOR DE NUMEROS PARES E IMPARES-----");
+                do{
+                    System.out.println("------Por favor imprima valores iguales de filas y columnas para continuar------");
+                    System.out.println("Indica la cantidad de filas: ");
+                    opcion_filas = sc.nextInt();
+                    System.out.println("Indica la cantidad de columnas: ");
+                    opcion_columnas = sc.nextInt();
+                }
+                while(opcion_filas != opcion_columnas);
             }
             default -> System.out.println("Por favor elija una opcion disponible");
         }
