a4f91b0cc4d20a70f9b5706fc4c661dc58cb69d6
alanbalfonso <a.bauzaa27@gmail.com>
2024-02-06 15:11:57 -0600

    llenado de arrays de diccionario

--- reto3.java
+++ reto3.java
@@ -1,6 +1,6 @@
 //Reto 3    **Diccionario y matriz**    Alan Bauza
-import java.io.*;
 import java.util.Scanner;
+import java.io.IOException;
 public class reto3 {
     public static void main(String[] args) throws IOException{
         Scanner sc = new Scanner(System.in);
@@ -13,7 +13,6 @@ public class reto3 {
             case 1->{
                 //programa de diccionario
                 char opcion_diccionario = 'x';
-
                 System.out.println("--------DICCIONARIO--------"
                 +"\nBienvenido al diccionario, escoge una opcion: "
                 +"\nA. Listar palabras del diccionario."
@@ -25,18 +24,20 @@ public class reto3 {
                     switch (opcion_diccionario) {
                         case 'A' ->{
                             //String que tiene un array en el que se guardan los datos del diccionario
-                            String [] palabras = {"lapiz", "microfono", "laptop", "servilleta", "arbol", "plato",
-                            "agua", "cable", "tijeras", "libreta"};
+                            String [] palabras = {"Lapiz", "Microfono", "Laptop", "Servilleta", "Arbol", "Plato", "Agua", "Cable", "Tijeras", "Libreta"};
                             //definiciones de cada palabra
                             String [] significados = {"Utensilio para escribir o dibujar formado por un cilindro o prisma de madera con una barra de grafito en su interior.",
                             "Aparato que transforma las ondas sonoras en corrientes eléctricas para su amplificacion.",
-                            "Computadora portatil de peso y tamanio ligero.", "Pieza de tela o papel que usa cada comensal para limpiarse los labios y las manos.",
-                            "Planta de tallo lenioso y elevado, que se ramifica a cierta altura del suelo.", "Pieza de la vajilla para comer.",
-                            "Liquido transparente, incoloro, inodoro e insipido en estado puro.", "Cordon formado con varios conductores aislados unos de otros y protegido"
-                            +" generalmente por una envoltura flexible y resistente.","Instrumento compuesto de dos hojas de acero, a manera de cuchillas de un solo filo.", 
+                            "Computadora portatil de peso y tamanio ligero.",
+                            "Pieza de tela o papel que usa cada comensal para limpiarse los labios y las manos.",
+                            "Planta de tallo lenioso y elevado, que se ramifica a cierta altura del suelo.",
+                            "Pieza de la vajilla para comer.",
+                            "Liquido transparente, incoloro, inodoro e insipido en estado puro.",
+                            "Cordon formado con varios conductores aislados unos de otros y protegido generalmente por una envoltura flexible y resistente.",
+                            "Instrumento compuesto de dos hojas de acero, a manera de cuchillas de un solo filo.", 
                             "Conjunto de hojas de papel, impresas o en blanco unidas que forman un libro delgado."};
                             System.out.println("-----ENLISTAR PALABRAS-----");
-                            
+
                         }
                         case 'B' ->{
     
