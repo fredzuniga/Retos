fb6d8a19aaf8695856b710a8ee3875a95a6ffef6
alanbalfonso <a.bauzaa27@gmail.com>
2024-02-10 13:25:09 -0600

    creacion de funcion enlistar_palabras()

--- reto3.java
+++ reto3.java
@@ -12,19 +12,7 @@ public class reto3 {
         switch (opcion) {
             case 1->{
                 //programa de diccionario
-                //String que tiene un array en el que se guardan los datos del diccionario
-                String [] palabras = {"Lapiz", "Microfono", "Laptop", "Servilleta", "Arbol", "Plato", "Agua", "Cable", "Tijeras", "Libreta"};
-                //definiciones de cada palabra
-                String [] significados = {"Utensilio para escribir o dibujar formado por un cilindro o prisma de madera con una barra de grafito en su interior.",
-                "Aparato que transforma las ondas sonoras en corrientes eléctricas para su amplificacion.",
-                "Computadora portatil de peso y tamanio ligero.",
-                "Pieza de tela o papel que usa cada comensal para limpiarse los labios y las manos.",
-                "Planta de tallo lenioso y elevado, que se ramifica a cierta altura del suelo.",
-                "Pieza de la vajilla para comer.",
-                "Liquido transparente, incoloro, inodoro e insipido en estado puro.",
-                "Cordon formado con varios conductores aislados unos de otros y protegido generalmente por una envoltura flexible y resistente.",
-                "Instrumento compuesto de dos hojas de acero, a manera de cuchillas de un solo filo.", 
-                "Conjunto de hojas de papel, impresas o en blanco unidas que forman un libro delgado."};
+
                 char opcion_diccionario = 'x';
                 System.out.println("--------DICCIONARIO--------" 
                 + "\nBienvenido al diccionario, escoge una opcion: ");
@@ -38,9 +26,10 @@ public class reto3 {
                     switch (opcion_diccionario) {
                         case 'A' ->{
                             System.out.println("-----ENLISTAR PALABRAS-----");
-                            for(int i = 0; i < palabras.length; i++){
-                                System.out.println("* " + palabras[i]);
-                            }
+                            reto3 lista = new reto3();  //objeto nuevo
+                            //mandar a llamar la clase
+                            funciones programa = lista.new funciones();
+                            programa.enlistar_palabras();   //usar enlistar_palabras()
                             System.out.println("-------------------");
                         }
                         //buscar una palabra del diccionario
@@ -48,6 +37,9 @@ public class reto3 {
                             String palabra_buscada;
                             System.out.println("-----BUSCAR PALABRA DENTRO DEL DICCIONARIO-----");
                             //ejecucion
+                            reto3 lista = new reto3();  //objeto nuevo
+                            funciones programa = lista.new funciones();
+                            programa.enlistar_palabras();
                             System.out.println("Escribe la palabra que quieres buscar en el diccionario: ");
                             palabra_buscada = sc.next();
                             
@@ -86,5 +78,25 @@ public class reto3 {
             default -> System.out.println("Por favor elija una opcion disponible");
         }
         sc.close();
+    }
+    public class funciones{
+        //String que tiene un array en el que se guardan los datos del diccionario
+        String [] palabras = {"Lapiz", "Microfono", "Laptop", "Servilleta", "Arbol", "Plato", "Agua", "Cable", "Tijeras", "Libreta"};
+        //definiciones de cada palabra
+        String [] significados = {"Utensilio para escribir o dibujar formado por un cilindro o prisma de madera con una barra de grafito en su interior.",
+        "Aparato que transforma las ondas sonoras en corrientes eléctricas para su amplificacion.",
+        "Computadora portatil de peso y tamanio ligero.",
+        "Pieza de tela o papel que usa cada comensal para limpiarse los labios y las manos.",
+        "Planta de tallo lenioso y elevado, que se ramifica a cierta altura del suelo.",
+        "Pieza de la vajilla para comer.",
+        "Liquido transparente, incoloro, inodoro e insipido en estado puro.",
+        "Cordon formado con varios conductores aislados unos de otros y protegido generalmente por una envoltura flexible y resistente.",
+        "Instrumento compuesto de dos hojas de acero, a manera de cuchillas de un solo filo.", 
+        "Conjunto de hojas de papel, impresas o en blanco unidas que forman un libro delgado."};
+        public void enlistar_palabras(){
+            for(int i = 0; i < palabras.length; i++){
+                System.out.println("* " + palabras[i]);
+            }
+        }
     }
 }
