407f42cc2dd924d48b111468f2c3c0d656ec363d
alanbalfonso <a.bauzaa27@gmail.com>
2024-02-10 20:54:38 -0600

    errores en programa prontos a arreglar

--- reto3.java
+++ reto3.java
@@ -2,6 +2,19 @@
 import java.util.Scanner;
 import java.io.IOException;
 public class reto3 {
+            //String que tiene un array en el que se guardan los datos del diccionario
+            String [] palabras = {"Lapiz", "Microfono", "Laptop", "Servilleta", "Arbol", "Plato", "Agua", "Cable", "Tijeras", "Libreta"};
+            //definiciones de cada palabra
+            String [] significados = {"Utensilio para escribir o dibujar formado por un cilindro o prisma de madera con una barra de grafito en su interior.",
+            "Aparato que transforma las ondas sonoras en corrientes eléctricas para su amplificacion.",
+            "Computadora portatil de peso y tamanio ligero.",
+            "Pieza de tela o papel que usa cada comensal para limpiarse los labios y las manos.",
+            "Planta de tallo lenioso y elevado, que se ramifica a cierta altura del suelo.",
+            "Pieza de la vajilla para comer.",
+            "Liquido transparente, incoloro, inodoro e insipido en estado puro.",
+            "Cordon formado con varios conductores aislados unos de otros y protegido generalmente por una envoltura flexible y resistente.",
+            "Instrumento compuesto de dos hojas de acero, a manera de cuchillas de un solo filo.", 
+            "Conjunto de hojas de papel, impresas o en blanco unidas que forman un libro delgado."};
     public static void main(String[] args) throws IOException{
         Scanner sc = new Scanner(System.in);
         int opcion;
@@ -16,6 +29,7 @@ public class reto3 {
                 char opcion_diccionario = 'x';
                 System.out.println("--------DICCIONARIO--------" 
                 + "\nBienvenido al diccionario, escoge una opcion: ");
+                reto3 lista = new reto3();  //objeto para enlistar palabras
                 while(opcion_diccionario != 'S'){
                     System.out.println("Escoge una opcion: "
                     +"\nA. Listar palabras del diccionario."
@@ -26,10 +40,8 @@ public class reto3 {
                     switch (opcion_diccionario) {
                         case 'A' ->{
                             System.out.println("-----ENLISTAR PALABRAS-----");
-                            reto3 lista = new reto3();  //objeto nuevo
                             //mandar a llamar la clase
-                            funciones programa = lista.new funciones();
-                            programa.enlistar_palabras();   //usar enlistar_palabras()
+                            
                             System.out.println("-------------------");
                         }
                         //buscar una palabra del diccionario
@@ -37,16 +49,14 @@ public class reto3 {
                             String palabra_buscada;
                             System.out.println("-----BUSCAR PALABRA DENTRO DEL DICCIONARIO-----");
                             //ejecucion
-                            reto3 lista = new reto3();  //objeto nuevo
-                            funciones programa = lista.new funciones();
-                            programa.enlistar_palabras();
+                            enlistar_palabras();
                             System.out.println("Escribe la palabra que quieres buscar en el diccionario: ");
                             palabra_buscada = sc.next();
+                            System.out.println("Palabra: " + palabra_buscada);
                             
-
                         }
                         case 'C' ->{
-    
+                            
                         }
                         case 'S' ->{
                             break;  //rompe el while
@@ -79,20 +89,15 @@ public class reto3 {
         }
         sc.close();
     }
-    public class funciones{
-        //String que tiene un array en el que se guardan los datos del diccionario
-        String [] palabras = {"Lapiz", "Microfono", "Laptop", "Servilleta", "Arbol", "Plato", "Agua", "Cable", "Tijeras", "Libreta"};
-        //definiciones de cada palabra
-        String [] significados = {"Utensilio para escribir o dibujar formado por un cilindro o prisma de madera con una barra de grafito en su interior.",
-        "Aparato que transforma las ondas sonoras en corrientes eléctricas para su amplificacion.",
-        "Computadora portatil de peso y tamanio ligero.",
-        "Pieza de tela o papel que usa cada comensal para limpiarse los labios y las manos.",
-        "Planta de tallo lenioso y elevado, que se ramifica a cierta altura del suelo.",
-        "Pieza de la vajilla para comer.",
-        "Liquido transparente, incoloro, inodoro e insipido en estado puro.",
-        "Cordon formado con varios conductores aislados unos de otros y protegido generalmente por una envoltura flexible y resistente.",
-        "Instrumento compuesto de dos hojas de acero, a manera de cuchillas de un solo filo.", 
-        "Conjunto de hojas de papel, impresas o en blanco unidas que forman un libro delgado."};
+    public class Diccionario{
+        private String[] palabras = new String[]{};
+        private String[] significados = new String[]{};
+
+        public Diccionario(String[] words, String[] meanings){ // ! Class constructor
+            this.palabras = words;
+            this.significados = meanings;
+        }
+
         public void enlistar_palabras(){
             for(int i = 0; i < palabras.length; i++){
                 System.out.println("* " + palabras[i]);
