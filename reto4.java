3d8bdea9ec8ae446bcaa74254f659bf0d87c7517
alanbalfonso <a.bauzaa27@gmail.com>
2024-02-27 08:28:40 -0600

    mejora y finalizacion del reto 4

--- reto4.java
+++ reto4.java
@@ -3,25 +3,21 @@
  * Por: Alan Bauza Alfonso
  */
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class reto4 {
+    private static ArrayList <Pasajero> pasajeros = new ArrayList<Pasajero>(); 
+    private static double precioBoleto = 1250.50;
+    int cantidadPasajerosTotal = 0;
     public static void main(String[] args) throws IOException{
         //objeto para incluir la funcion iniciar()
         reto4 v = new reto4();
-        Venta programa = v.new Venta("", 0);
+        Venta programa = v.new Venta();
         programa.iniciar();
         }
 
         public class Venta {
-            private String nombre_comprador;
-            private int cantidad_boletos;
             char opcion = 'c';
-
-            //constructor
-            Venta(String nombre_comprador, int cantidad_boletos){
-                this.nombre_comprador = nombre_comprador;
-                this.cantidad_boletos = cantidad_boletos;
-            }
             
             /*
              * Iniciar el sistema de registro de ventas de boletos
@@ -34,17 +30,52 @@ public class reto4 {
                     opcion = sc.next().charAt(0);
                     switch (opcion) {
                         case 'A' ->{
+                            int cantidadPasajerosVenta = 1;
                             System.out.println("----VENTA DE BOLETO----");
                             System.out.println("Ingrese el nombre del comprador: ");
-
-                            //el atributo nombre_comprador cambiara cuando el comprador escriba su nombre
-                            nombre_comprador = sc.next();
+                            String nombre_comprador = sc.next();
                             System.out.println("Ingrese el numero de pasajeros adicionales: ");
-                            cantidad_boletos = sc.nextInt();
-                            
+                            int cantidad_pasajeros = sc.nextInt();
+                            //objeto comprador donde se guarda la cantidad de pasajeros adicionales y sus nombres
+                            Pasajero comprador = new Pasajero();
+                            comprador.nombrePasajero = nombre_comprador;
+                            //creacion de vector que tiene el tamaño de la cantidad_pasajeros
+                            String[] nombre_pasajeros_adicionales = new String[cantidad_pasajeros];
+                            for(int i = 0; i < cantidad_pasajeros; i++){
+                                System.out.println("Ingrese el nombre del pasajero adicional " + (i+1));
+                                nombre_pasajeros_adicionales[i] = sc.next();
+                            }
+                            //agregar los pasajeros adicionales
+                            comprador.pasajerosAdicionales = nombre_pasajeros_adicionales;
+                            pasajeros.add(comprador);
+                            cantidadPasajerosVenta += cantidad_pasajeros;
+                            /*
+                             * Impresion de ticket de venta
+                             */
+                            System.out.println("------Ticket de venta------");
+                            System.out.println("Cantidad de boletos: " + cantidadPasajerosVenta);
+                            System.out.println("Importe de venta: $" + cantidadPasajerosVenta * precioBoleto);
+                            System.out.println("Nombre del comprador: " + nombre_comprador);
+                            System.out.println("Pasajeros adicionales: " + comprador.pasajerosAdicionales.length);
+                            //conteo de pasajeros totales
+                            cantidadPasajerosTotal += cantidadPasajerosVenta;   
                         }
                         case 'B' ->{
-                            System.out.println("B");
+                            /*
+                             * Impresion de la lista de pasajeros
+                             */
+                            int conteo = 0;
+                            System.out.println("-----LISTA DE PASAJEROS-----");
+                            for(Pasajero comprador : pasajeros){
+                                conteo++;
+                                System.out.println("Pasajero " + conteo + " (comprador): " + comprador.nombrePasajero);
+                                for(int i = 0; i < comprador.pasajerosAdicionales.length; i++){
+                                    conteo++;
+                                    System.out.println("Pasajero " + conteo + " " + comprador.pasajerosAdicionales[i]);
+                                }
+                            }
+                            System.out.println("Total de pasajeros: " + cantidadPasajerosTotal);
+                            System.out.println("Monto total de ventas: $" + cantidadPasajerosTotal * precioBoleto);
                         }
                         case 'S' ->{
                             System.out.println("----FIN DEL PROGRAMA----");
@@ -64,5 +95,9 @@ public class reto4 {
             public void iniciar(){
                 registro_ventas();
             }
+        }
+        class Pasajero{
+            public String nombrePasajero;
+            public String[] pasajerosAdicionales;
         }
     }
