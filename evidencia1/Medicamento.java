c710c785898a616ed10736b35e75a4e6503c695f
alanbalfonso <a.bauzaa27@gmail.com>
2024-02-29 08:33:07 -0600

    creacion de getters y set, objetos

--- evidencia1/Medicamento.java
+++ evidencia1/Medicamento.java
@@ -1,5 +1,82 @@
 package evidencia1;
 
 public class Medicamento {
+    private String nombreQuimico;
+    private String nombreGenerico;
+    private String nombreRegistrado;
+    private double precioPublico;
+    //private double precioVenta;
+    private int formaFarmaceutica;
+
+    /*
+     * seleccionar todas las variables y crear constructor con el foquito
+     */
+    public Medicamento(String nombreQuimico, String nombreGenerico, String nombreRegistrado, double precioPublico,
+            double precioVenta, int formaFarmaceutica) {
+        this.nombreQuimico = nombreQuimico;
+        this.nombreGenerico = nombreGenerico;
+        this.nombreRegistrado = nombreRegistrado;
+        this.precioPublico = precioPublico;
+        this.formaFarmaceutica = formaFarmaceutica;
+        //this.precioVenta = precioVenta;
+    }
+
+    //constructor vacio
+    public Medicamento(){}
+
+    //constructor exclusivo del nombreQuimico
+    public Medicamento(String nombreQuimico) {
+        this.nombreQuimico = nombreQuimico;
+    }
+
+    public String getNombreQuimico() {
+        return nombreQuimico;
+    }
+    public void setNombreQuimico(String nombreQuimico) {
+        this.nombreQuimico = nombreQuimico;
+    }
+    public String getNombreGenerico() {
+        return nombreGenerico;
+    }
+    public void setNombreGenerico(String nombreGenerico) {
+        this.nombreGenerico = nombreGenerico;
+    }
+    public String getNombreRegistrado() {
+        return nombreRegistrado;
+    }
+    public void setNombreRegistrado(String nombreRegistrado) {
+        this.nombreRegistrado = nombreRegistrado;
+    }
+    public double getPrecioPublico() {
+        return precioPublico;
+    }
+    public void setPrecioPublico(double precioPublico) {
+        this.precioPublico = precioPublico;
+    }
+    /*public double getPrecioVenta() {
+        //TODO modificar para devolver con porcentaje
+        return precioVenta;
+    }*/
+    public double getPrecioVentaPorcentaje(){
+        double precioFinal = 0;
+        switch(formaFarmaceutica){
+            case 1 ->{
+                precioFinal = precioPublico * 0.09;
+            }
+            case 2 ->{
+                precioFinal = precioPublico * 0.12;
+            }
+            case 3 ->{
+                precioFinal = precioPublico * 0.13;
+            }
+        }
+        return precioFinal;
+    }
+    public int getFormaFarmaceutica() {
+        return formaFarmaceutica;
+    }
+    public void setFormaFarmaceutica(int formaFarmaceutica) {
+        this.formaFarmaceutica = formaFarmaceutica;
+    }   
     
 }
--- evidencia1/evidencia1.java
+++ evidencia1/evidencia1.java
@@ -8,22 +8,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-//!importar la clase medicamento para la evidencia
+//importar la clase medicamento para la evidencia
 import evidencia1.Medicamento;
+
 public class evidencia1 {
     //Arraylist del tipo de clase Medicamento
-    private static ArrayList <Medicamento> medicamentos = new ArrayList <Medicamento>();
+    private static ArrayList <Medicamento> listaMedicamentos = new ArrayList <Medicamento>();
     public static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) {
         /*
          * Empieza el programa sin usuario o contrasenia
-         * TODO agregar el inicio de sesion al programa
+         * //TODO agregar el inicio de sesion al programa
+         * //TODO terminar caso A de agregar medicamentos
          */
-        char opcion = 'p';
+        char opcion = ' ';
         System.out.println("Evidencia 1 --- Registro de medicamentos");
         do{
             System.out.println("Indica una opcion. \nA. Registrar medicamento \nS. Salir del programa");
-            opcion = entrada.readLine().charAt(0);
+            try {
+                opcion = entrada.readLine().charAt(0);
+                switch(opcion){
+                    case 'A' ->{
+                        System.out.println("Indica el nombre quimico: ");
+                        String nombreQuimico = entrada.readLine();
+                        Medicamento medicamento = new Medicamento(nombreQuimico);
+                        listaMedicamentos.add(medicamento);
+                    }
+                    case 'S' ->{
+
+                    }
+                    default -> System.out.println("Escoje una opcion valida.");
+                }
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
         }while(opcion != 'S');
     }
 }
