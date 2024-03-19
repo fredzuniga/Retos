8bfd01c277b41c7c1e480e12411d709739d77de5
alanbalfonso <a.bauzaa27@gmail.com>
2024-03-19 10:28:18 -0600

    si

---
+++ reto6.java
@@ -0,0 +1,19 @@
+/*
+ * Por: Alan Bauza Alfonso
+ * Reto 6: Nomina basica de empleados
+ */
+import java.util.ArrayList;
+public class reto6 {
+    public static void main(String[] args) {
+        ArrayList <Object> empleadosRegistrados = new ArrayList<>();
+
+    }
+
+    private class Empleados{
+        String nombreCompleto;
+        int horasTrabajadas;
+        int horasTrabajadasExtras;
+        int telefono;
+        // ...
+    }
+}
---
+++ tema5/GroseriaDetectada.java
@@ -0,0 +1,8 @@
+package tema5;
+
+//definir una excepcion
+class GroseriaDetectada extends Exception{
+    public GroseriaDetectada (String message){
+        super(message);
+    }
+}
