b0cdc051fa988a813041ee39162abf8494e5b073
alanbalfonso <a.bauzaa27@gmail.com>
2024-04-02 06:45:22 -0600

    mejora reto6

--- reto6.java
+++ reto6.java
@@ -2,9 +2,13 @@
  * Por: Alan Bauza Alfonso
  * Reto 6: Nomina basica de empleados
  */
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
+
 public class reto6 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         ArrayList <Object> empleadosRegistrados = new ArrayList<>();
 
     }
@@ -14,6 +18,7 @@ public class reto6 {
         int horasTrabajadas;
         int horasTrabajadasExtras;
         int telefono;
-        // ...
+        Date fechaNacimiento;
+        private static DateTimeFormatter fecha = DateTimeFormatter.ofPattern("dd/mm/aa");
     }
 }
